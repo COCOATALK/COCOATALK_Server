72238abe1a5054d17ef3b4a8628d6e42499c843c
ChoHadam <hadamcho97@gmail.com>
2020-07-01 17:12:22 +0900

    Common Module 추가

--- cocoatalk_server/routes/index.js
+++ cocoatalk_server/routes/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use('/users', require('./users'));
+router.use('/chat', require('./Chat'));
 
 module.exports = router;
